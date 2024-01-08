67251532da84cc2defdd27b81a9c26b05852dac2
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-01-08 00:38:35 -0500

    feat: update addresses

--- cmd/analytics/main.go
+++ cmd/analytics/main.go
@@ -71,27 +71,26 @@ func analyzeTries(ctx context.Context, trieRoot common.Hash, statedb state.Datab
 	// histStatePathTypes := histogram.New[string]("State Trie - Path types")
 
 	_addresses := [...]string{
-		"0xdAC17F958D2ee523a2206206994597C13D831ec7", // USDT
-		"0x06450dee7fd2fb8e39061434babcfc05599a6fb8", // XEN
+		// "0x06450dee7fd2fb8e39061434babcfc05599a6fb8", // XEN
+		"0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
 		"0x5acc84a3e955bdd76467d3348077d003f00ffb97", // Forsage
-		"0x00000000006c3852cbef3e08e8df289169ede581", // Seaport 1.1
-		"0x2a0c0dbecc7e4d658f48e01e3fa353f44050c208", // IDEX
-		"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
-		"0x1a2a1c938ce3ec39b6d47113c7955baa9dd454f2", // Axie
-		"0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85", // ENS
-		"0x39755357759ce0d7f32dc8dc45414cca409ae24e", // Maker
-		"0x06012c8cf97bead5deae237070f9587f8e7a266d", // CryptoKitties
-		"0x00000000000c2e074ec69a0dfb2997ba6c7d2e1e", // ENS Registry
-		"0x495f947276749ce646f68ac8c248420045cb7b5e", // OpenSea storefront
-		"0x0ba45a8b5d5575935b8158a88c631e9f9c95a2e5", // TellorMaster
-		"0x000000000000ad05ccc4f10045630fb830b95127", // Blur
-		"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
-		"0x06a6a7af298129e3a2ab396c9c06f91d3c54aba8", // 0xUniverse Galaxy
-		"0x283af0b28c62c092c9727f1ee09c02ca627eb7f5", // ENS ETH Registrar
-		"0x080bf510fcbf18b91105470639e9561022937712", // 0x Exchange
-		"0x514910771af9ca656af840dff83e8264ecf986ca", // LINK
-		"0x629a673a8242c2ac4b7b8c5d8735fbeac21a6205", // SorareTokens
-		"0x58b6a8a3302369daec383334672404ee733ab239", // LPT
+		"0x00000000006c3852cbef3e08e8df289169ede581",
+		"0x7be8076f4ea4a4ad08075c2508e481d6c946d12b",
+		"0x2a0c0dbecc7e4d658f48e01e3fa353f44050c208",
+		"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+		"0x1a2a1c938ce3ec39b6d47113c7955baa9dd454f2",
+		"0x8a91c9a16cd62693649d80afa85a09dbbdcb8508",
+		"0x7f268357a8c2552623316e2562d90e642bb538e5",
+		"0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9",
+		"0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85",
+		"0x39755357759ce0d7f32dc8dc45414cca409ae24e",
+		"0x06012c8cf97bead5deae237070f9587f8e7a266d",
+		"0x00000000000c2e074ec69a0dfb2997ba6c7d2e1e",
+		"0x495f947276749ce646f68ac8c248420045cb7b5e",
+		"0x8d12a197cb00d4747a1fe03395095ce2a5cc6819",
+		"0x8853b05833029e3cf8d3cbb592f9784fa43d2a79",
+		"0x0ba45a8b5d5575935b8158a88c631e9f9c95a2e5",
+		"0xbcf935d206ca32929e1b887a07ed240f0d8ccd22",
 	}
 
 	addresses := make([]common.Address, len(_addresses))
